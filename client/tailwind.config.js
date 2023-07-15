64585b23d144cd8c45017b6b0ab38608ef024425
gulphater <dotanoobbich@mail.ru>
2023-07-15 18:39:37 -0400

    Navbar Fronted Finished. Installing react router dom

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -5,7 +5,13 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: { 
+      colors: { 
+        'primary': {
+          DEFAULT: "#F5385D"
+        }
+      }
+    }
   },
   plugins: [],
 }
